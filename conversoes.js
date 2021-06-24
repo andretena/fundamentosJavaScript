8f2b24d3638c5cb56709f0aa6212e2e7293b278b
andretena <andretena@gmail.com>
2021-06-24 11:13:36 -0300

    codigos aula 2

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+// conversão implicita.
+
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero == numeroString);
+
+console.log(numero + numeroString);
+
+//conversão explicita.
+
+//Number
+console.log(numero + Number(numeroString));
+
+//String
---
+++ truthy-falsy.js
@@ -0,0 +1,23 @@
+// boolean
+const usuarioLogado = true;
+const contapaga = false;
+
+//truthy or falsy
+console.log(0 == false);
+console.log(1 == false);
+
+// null == vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero);
+console.log(typeof texto);
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
+// undefined
